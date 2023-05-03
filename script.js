be7d626e7cd55d2152066d00e09333c3c9ed5bf5
rona chen <rona.chen@hotmail.com>
2023-05-03 17:32:39 -0400

    added if statements for invalid inputs for password length and invalid selections for pw criteria

--- script.js
+++ script.js
@@ -17,9 +17,24 @@ function writePassword() {
   var specialCharacters = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
 
   // Prompts users to input a desired password length
-  var passwordLength = window.prompt("Enter your desired password length with at least 8 characters and no more than 128 characters.");
+  var passwordLength = window.prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
 
-  console.log(passwordLength);
+  // If the user enters an invalid number, then an error would show up that redirect thems to enter a valid number
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a valid password length!\nPlease enter a number anywhere from 8 to 128:");
+    var passwordLength = window.prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
+  }
+
+  // If the user selects "cancel" for all prompts, then an error would show up that redirects them back to the prompts
+  if (uppercase === false && lowercase === false && numbers === false && specialCharacters === false) {
+    window.alert("You must select at least one criteria!\nPlease select OK for at least one of the following prompts:");
+    var uppercase = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
+    var lowercase = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
+    var numbers = window.confirm("Do you want numbers (i.e., 123) in your password?");
+    var specialCharacters = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
+  }
+
+  console.log (specialCharacters);
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
