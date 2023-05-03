0b4bc35c8466a10edd61f9f1d61344b2e7b97841
rona chen <rona.chen@hotmail.com>
2023-05-03 18:37:51 -0400

    moved the alert prompts in the js file and also added a variable that concat selected arrays

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+window.alert("Click on the red button to generate your password!")
+
 // The arrays for uppercase, lowercase, numbers, and special characters
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -11,10 +13,19 @@ var specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', 
 function writePassword() {
 
   // Prompts that allow users to click "ok" for yes or "cancel" for no
-  var uppercase = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
-  var lowercase = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
-  var numbers = window.confirm("Do you want numbers (i.e., 123) in your password?");
-  var specialCharacters = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
+  var uppercaseTrue = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
+  var lowercaseTrue = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
+  var numbersTrue = window.confirm("Do you want numbers (i.e., 123) in your password?");
+  var specialCharactersTrue = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
+
+  // If the user selects "cancel" for all prompts, then an error would show up that redirects them back to the prompts
+  if (uppercase === false && lowercase === false && numbers === false && specialCharacters === false) {
+    window.alert("You must select at least one criteria!\nPlease select OK for at least one of the following prompts:");
+    var uppercaseTrue = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
+    var lowercaseTrue = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
+    var numbersTrue = window.confirm("Do you want numbers (i.e., 123) in your password?");
+    var specialCharactersTrue = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
+  }
 
   // Prompts users to input a desired password length
   var passwordLength = window.prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
@@ -25,16 +36,18 @@ function writePassword() {
     var passwordLength = window.prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
   }
 
-  // If the user selects "cancel" for all prompts, then an error would show up that redirects them back to the prompts
-  if (uppercase === false && lowercase === false && numbers === false && specialCharacters === false) {
-    window.alert("You must select at least one criteria!\nPlease select OK for at least one of the following prompts:");
-    var uppercase = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
-    var lowercase = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
-    var numbers = window.confirm("Do you want numbers (i.e., 123) in your password?");
-    var specialCharacters = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
-  }
+  // Need to add the selected criteria array(s) together using .concat method, and overwrites everytime if the criteria is 
+  var selectedCriteria = []
 
-  console.log (specialCharacters);
+  if (uppercaseTrue) {
+    selectedCriteria.concat(uppercase);
+  } else if (lowercaseTrue) {
+    selectedCriteria.concat(lowercase);
+  } else if (numbersTrue) {
+    selectedCriteria.concat(numbers);
+  } else (specialCharactersTrue) 
+    selectedCriteria.concat(specialCharacters);
+
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
