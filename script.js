cbc423762cd32da8ce798fa72307f4494b8a9709
rona chen <rona.chen@hotmail.com>
2023-05-04 00:18:30 -0400

    formatted the codes that executed correctly

--- script.js
+++ script.js
@@ -12,86 +12,69 @@ var specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', 
 var selectedCharacters = [];
 
 // Set max and min length values
-var min = 8; 
+var min = 8;
 var max = 128;
 
-   
-    
+// Sets a password variable with no values
+var password = ""
+
 // Writes the function for generatePassword
 function generatePassword() {
 
-  // Prompts users to input a desired password length
-  var passwordLength = prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128)."); 
-  
-  // If the user enters an invalid number, then an error would show up that redirect thems to enter a valid number
+  // Prompt users to choose what characters to include in their password
+  var uppercaseTrue = confirm("Do you want uppercase letters (i.e., ABC) in your password?");
+  var lowercaseTrue = confirm("Do you want lowercase letters (i.e., abc) in your password?");
+  var numbersTrue = confirm("Do you want numbers (i.e., 123) in your password?");
+  var specialCharactersTrue = confirm("Do you want special characters (i.e., !@#$%^) in your password?");
+  // If the user selects "cancel" for all prompts, the function will terminate
+  if (uppercaseTrue === false && lowercaseTrue === false && numbersTrue === false && specialCharactersTrue === false) {
+    return "You must select at least one password criteria.\nPlease try again!";
+  }
+
+  // Prompt users to input a desired password length
+  var passwordLength = prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
+  // If the user enters an invalid number, it will prompt the user to enter a valid number, if not the function will terminate after two inputs of invalid numbers
   if (passwordLength < min || passwordLength > max) {
-      prompt("Please enter a valid password length!\nEnter a number from 8 to 128:");
-    } else if (isNaN(passwordLength)) {
-      prompt("Please enter a valid password length!\nEnter a number from 8 to 128:");
+    prompt("Please enter a valid password length!\nEnter a number from 8 to 128:");
+  } else if (isNaN(passwordLength)) {
+    prompt("Please enter a valid password length!\nEnter a number from 8 to 128:");
     return "You need to enter a valid password length. Please try again!";
   }
-    
-  // Prompts that allow users to choose what characters to include in their password
-  var uppercaseTrue = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
-  var lowercaseTrue = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
-  var numbersTrue = window.confirm("Do you want numbers (i.e., 123) in your password?");
-  var specialCharactersTrue = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
- 
- 
- 
-  //
 
-
-  // If the user selects "cancel" for all prompts, then an error would show up that redirects them back to the prompts
-  if (!uppercase && !lowercase && !numbers && !specialCharacters) {
-    window.alert("You must select at least one criteria!\nPlease select OK for at least one of the following prompts:");
-    var uppercaseTrue = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
-    var lowercaseTrue = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
-    var numbersTrue = window.confirm("Do you want numbers (i.e., 123) in your password?");
-    var specialCharactersTrue = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
-  }
-
-  
-
-  
-
-  // Need to add the selected criteria array(s) together using .concat method, and overwrites everytime if the criteria is true
-  var selectedCriteria = [];
-
+  // Need to add the selected criteria arrays together using .concat method, which overwrites everytime when the criteria is true
   if (uppercaseTrue) {
-    selectedCriteria = selectedCriteria.concat(uppercase);
-  } 
-  
+    selectedCharacters = selectedCharacters.concat(uppercase);
+  }
+
   if (lowercaseTrue) {
-    selectedCriteria = selectedCriteria.concat(lowercase);
-  } 
-  
+    selectedCharacters = selectedCharacters.concat(lowercase);
+  }
+
   if (numbersTrue) {
-    selectedCriteria = selectedCriteria.concat(numbers);
-  } 
-  
+    selectedCharacters = selectedCharacters.concat(numbers);
+  }
+
   if (specialCharactersTrue) {
-    selectedCriteria = selectedCriteria.concat(specialCharacters);
+    selectedCharacters = selectedCharacters.concat(specialCharacters);
   }
 
-  // Make the selectedCriteria random, the randomization would continue to select a random character from the selectedCriteria array up until the desired passwordLength
-  var password = "";
+//   // Randomization would continue to select a random character from the selected characters until the desired passwordLength
+//   for (var x = 0; x < passwordLength; x++); {
+//     y = selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
+//   }
 
-  for (var x = 0; x < passwordLength; x++); {
-    password = selectedCriteria[Math.floor(Math.random() * selectedCriteria.length)];
-  }
+//   // Need to add the previously defined value of password to the created randomized string of password
+//   var password = password.join(y);
 
-  return password;
-
-}
+//   return password;
+// }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); 
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
 }
 
 // Add event listener to generate button
