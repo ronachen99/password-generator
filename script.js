d6269534265ae9a4774f6077d8d653704774cb32
rona chen <rona.chen@hotmail.com>
2023-05-03 22:56:22 -0400

    changed the if and else statement for invalid input for pw length

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 // Assignment Code
-
 var generateBtn = document.querySelector("#generate");
 
 // Directs the end user to click on the generate button
 window.alert("Click on the red button to generate your password!");
 
-// The arrays for uppercase, lowercase, numbers, special characters, and selected characters by the end user
+// The arrays for uppercase, lowercase, numbers, special characters, and selected characters  that were chosen by the end user
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 var selectedCharacters = [];
 
+// Set max and min length values
+var min = 8; 
+var max = 128;
+
+   
+    
 // Writes the function for generatePassword
 function generatePassword() {
 
-  // Prompts that allow users to click "ok" for yes or "cancel" for no
+  // Prompts users to input a desired password length
+  var passwordLength = prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128)."); 
+  
+  // If the user enters an invalid number, then an error would show up that redirect thems to enter a valid number
+  if (passwordLength < min || passwordLength > max) {
+      prompt("Please enter a valid password length!\nEnter a number from 8 to 128:");
+    } else if (isNaN(passwordLength)) {
+      prompt("Please enter a valid password length!\nEnter a number from 8 to 128:");
+    return "You need to enter a valid password length. Please try again!";
+  }
+    
+  // Prompts that allow users to choose what characters to include in their password
   var uppercaseTrue = window.confirm("Do you want uppercase letters (i.e., ABC) in your password?");
   var lowercaseTrue = window.confirm("Do you want lowercase letters (i.e., abc) in your password?");
   var numbersTrue = window.confirm("Do you want numbers (i.e., 123) in your password?");
   var specialCharactersTrue = window.confirm("Do you want special characters (i.e., !@#$%^) in you password?");
  
-  // Prompts users to input a desired password length
-  var passwordLength = window.prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
+ 
  
   //
 
@@ -38,11 +53,7 @@ function generatePassword() {
 
   
 
-  // If the user enters an invalid number, then an error would show up that redirect thems to enter a valid number
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please enter a valid password length!\nPlease enter a number anywhere from 8 to 128:");
-    var passwordLength = window.prompt("The password must be at least 8 characters and no more than 128 characters. Please enter your desired password length (i.e., 8-128).");
-  }
+  
 
   // Need to add the selected criteria array(s) together using .concat method, and overwrites everytime if the criteria is true
   var selectedCriteria = [];
@@ -72,7 +83,7 @@ function generatePassword() {
 
   return password;
 
-  }; 
+}
 
 // Write password to the #password input
 function writePassword() {
